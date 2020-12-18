5e71f62b46c485f3f6ce32efee98a808891f2255
SeoYangHun <syh8088@docfriends.com>
2020-12-19 08:50:07 +0900

    Update GlobalExceptionController.java

--- src/main/java/example/validator/error/exception/GlobalExceptionController.java
+++ src/main/java/example/validator/error/exception/GlobalExceptionController.java
@@ -1,5 +1,6 @@
 package example.validator.error.exception;
 
+import example.validator.common.CommonResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,13 +13,16 @@ public class GlobalExceptionController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = MemberException.class) // 클래스 단위로도 사용 가능(필요한 컨트롤러 내에 선언)
     @ResponseBody
-    public String handleBaseException(MemberException e) {
-        System.out.println(e.getErrorCode());
+    public CommonResponse handleBaseException(MemberException e) {
         System.out.println(e.getMessage());
+        System.out.println(e.getErrorCode());
         /*
             여기서 원하는 형태로 response 하면 됩니다!!!!
          */
-        return e.getErrorCode();
+        return CommonResponse.builder()
+                .errorCode(e.getErrorCode())
+                .message(e.getMessage())
+                .build();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
